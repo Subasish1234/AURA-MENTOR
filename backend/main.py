37b4cf7a62b158dad4956a4ee6665a23c5364d08
unknown <subashishmeher49@gmail.com>
2026-06-14 16:10:36 +0530

    Phase 1.3: Create users API router

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
+from app.database.database import engine
+from app.database.database import Base
+
+import app.database.models
+
+from app.api.users import router as user_router
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="AURA API")
+
+app.include_router(user_router)
+
 
 @app.get("/")
 def root():
-    return {"message": "AURA Backend Running"}
+    return {
+        "message": "AURA Backend Running"
+    }
